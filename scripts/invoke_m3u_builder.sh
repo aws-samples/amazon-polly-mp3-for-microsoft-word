e94174b046f45e72fc774ef87debbca3e5608c46
vsnyc <vinod.k.shukla@gmail.com>
2019-05-31 11:09:59 -0400

    Added region to aws cloudformation cli commands

--- scripts/invoke_m3u_builder.sh
+++ scripts/invoke_m3u_builder.sh
@@ -4,7 +4,7 @@ REGION=$1
 POLLY_ASSETS_BUCKET=$2
 STACK_NAME=$3
 
-M3U_FUNCTION_NAME=$(aws cloudformation describe-stacks --stack $STACK_NAME --output text | grep OUTPUTS | grep m3uBuilderFunctionName | cut -f4)
+M3U_FUNCTION_NAME=$(aws cloudformation --region ${REGION} describe-stacks --stack $STACK_NAME --output text | grep OUTPUTS | grep m3uBuilderFunctionName | cut -f4)
 
 aws lambda invoke --function-name ${M3U_FUNCTION_NAME} \
   --payload "{\"s3Bucket\": \"${POLLY_ASSETS_BUCKET}\", \"m3uFile\": \"polly-faq-reader/mp3/polly-faq.m3u\"}" \
